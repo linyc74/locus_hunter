889df477c9cfc17904bf4304332e62b1c478c8ce
linyc74 <ylin@nycu.edu.tw>
2023-12-01 23:58:47 +0800

    refactor view_loci.py

--- locus_hunter/view_loci.py
+++ locus_hunter/view_loci.py
@@ -2,51 +2,108 @@ import matplotlib.pyplot as plt
 from typing import Optional, List
 from ngslite import GenericFeature, Chromosome
 from dna_features_viewer import GraphicFeature, GraphicRecord
-from .template import Processor, Settings
 from .constant import COLOR_KEY
+from .template import Processor, Settings
+
+
+class ViewLoci(Processor):
+
+    loci: List[Chromosome]
+    output: str
+
+    graphic_records: List[GraphicRecord]
+
+    def main(
+            self,
+            loci: List[Chromosome],
+            output: str):
+
+        self.loci = loci
+        self.output = output
+
+        self.set_graphic_records()
+        self.plot_graphic_records()
+
+    def set_graphic_records(self):
+        to_graphic_record = ChromosomeToGraphicRecord(self.settings).main
+        self.graphic_records = [to_graphic_record(locus) for locus in self.loci]
+
+    def plot_graphic_records(self):
+        seqnames = [locus.seqname for locus in self.loci]
+        PlotGraphicRecords(self.settings).main(
+            graphic_records=self.graphic_records,
+            seqnames=seqnames,
+            output=self.output)
+
+
+class ChromosomeToGraphicRecord(Processor):
+
+    FEATURE_TYPES = ['CDS']
+
+    chromosome: Chromosome
+
+    graphic_features: List[GraphicFeature]
+
+    def main(self, chromosome: Chromosome) -> GraphicRecord:
+        self.chromosome = chromosome
+
+        self.set_graphic_features()
+
+        return GraphicRecord(
+            sequence_length=len(self.chromosome.sequence),
+            sequence=None,
+            features=self.graphic_features,
+            feature_level_height=1,
+            first_index=0,
+            plots_indexing='biopython',
+            labels_spacing=8,
+            ticks_resolution='auto')
+
+    def set_graphic_features(self):
+        to_graphic_feature = GenericToGraphicFeature(self.settings).main
+        self.graphic_features = [
+            to_graphic_feature(f) for f in self.chromosome.features
+            if f.type in self.FEATURE_TYPES
+        ]
 
 
 class GenericToGraphicFeature(Processor):
 
-    generic_feature: GenericFeature
-
-    strand_dict = {
+    STRAND_DICT = {
         '+': 1,
         '-': -1
     }
+    DEFAULT_COLOR = '#FFFFFF'
+    LABEL = None
+    THICKNESS = 8.0
+    LINEWIDTH = 0.5
+    LINECOLOR = '#000000'
+    OPEN_LEFT = False
+    OPEN_RIGHT = False
 
-    color_key: str = COLOR_KEY
-    default_color: str = '#FFFFFF'
+    generic_feature: GenericFeature
 
-    label: Optional[str] = None
-    thickness: float = 8.
-    linewidth: float = .5
-    linecolor: str = '#000000'
-    open_left: bool = False
-    open_right: bool = False
-
-    def __init__(self, settings: Settings):
-        super().__init__(settings=settings)
+    graphic_feature: GraphicFeature
 
     def main(self, generic_feature: GenericFeature) -> GraphicFeature:
         self.generic_feature = generic_feature
-        return self.get_graphic_feature()
+        self.set_graphic_feature()
+        return self.graphic_feature
 
-    def get_graphic_feature(self) -> GraphicFeature:
-        f = self.generic_feature
-        return GraphicFeature(
-            start=f.start,
-            end=f.end,
+    def set_graphic_feature(self):
+        self.graphic_feature = GraphicFeature(
+            start=self.generic_feature.start,
+            end=self.generic_feature.end,
             strand=self.get_strand(),
-            label=self.label,
+            label=self.LABEL,
             color=self.get_color(),
-            thickness=self.thickness,
-            linewidth=self.linewidth,
-            linecolor=self.linecolor,
+            thickness=self.THICKNESS,
+            linewidth=self.LINEWIDTH,
+            linecolor=self.LINECOLOR,
             fontdict=None,
             html=None,
-            open_left=self.open_left,
-            open_right=self.open_right,
+            open_left=self.OPEN_LEFT,
+            open_right=self.OPEN_RIGHT,
             box_linewidth=1,
             box_color='auto',
             legend_text=None,
@@ -54,67 +111,30 @@ class GenericToGraphicFeature(Processor):
 
     def get_strand(self) -> int:
         f = self.generic_feature
-        return self.strand_dict.get(f.strand, 0)
+        return self.STRAND_DICT.get(f.strand, 0)
 
     def get_color(self) -> str:
         f = self.generic_feature
         color = f.get_attribute(key=COLOR_KEY)
         if color is None:
-            color = self.default_color
+            color = self.DEFAULT_COLOR
         return color
 
 
-class ChromosomeToGraphicRecord(Processor):
-
-    chromosome: Chromosome
-
-    feature_types = ['CDS']
-
-    def __init__(self, settings: Settings):
-        super().__init__(settings=settings)
-        self.to_graphic_feature = GenericToGraphicFeature(self.settings).main
-
-    def main(self, chromosome: Chromosome) -> GraphicRecord:
-        self.chromosome = chromosome
-
-        return GraphicRecord(
-            sequence_length=len(self.chromosome.sequence),
-            sequence=None,
-            features=self.get_graphic_features(),
-            feature_level_height=1,
-            first_index=0,
-            plots_indexing='biopython',
-            labels_spacing=8,
-            ticks_resolution='auto')
-
-    def get_graphic_features(self) -> List[GraphicFeature]:
-        generic_features = self.chromosome.features
-        return [self.to_graphic_feature(f) for f in generic_features
-                if f.type in self.feature_types]
-
-
-def to_inch(cm) -> float:
-    return cm / 2.54
-
-
 class PlotGraphicRecords(Processor):
+
+    HEIGHT_CM_PER_LOCUS = 1 / 2.54
+    WIDTH_CM_PER_KB = 1 / 2.54
+    WIDTH_CM_PER_CHAR = 0.2 / 2.54
+    SCALE_BAR_KB = 5000
+    DPI = 600
 
     graphic_records: List[GraphicRecord]
     seqnames: List[str]
     output: str
 
-    height_cm_per_locus: float = to_inch(1)
-    width_cm_per_kb: float = to_inch(1)
-    width_cm_per_char: float = to_inch(0.2)
-    scale_bar_kb: int = 5000
-    dpi = 600
-
     figure: plt.Figure
     axs = List[plt.Axes]
-
-    def __init__(self, settings: Settings):
-        super().__init__(settings=settings)
-        self.to_graphic_record = ChromosomeToGraphicRecord(self.settings).main
 
     def main(
             self,
@@ -181,17 +201,17 @@ class PlotGraphicRecords(Processor):
 
     def set_scale_bar(self):
         ax = self.axs[-1]
-        ax.set_xticks([0, self.scale_bar_kb])
+        ax.set_xticks([0, self.SCALE_BAR_KB])
         ax.tick_params(axis='x', which='major', labelsize='small')
 
     def set_figure_size(self):
         w = self.get_figure_width()
-        h = self.height_cm_per_locus * len(self.graphic_records)
+        h = self.HEIGHT_CM_PER_LOCUS * len(self.graphic_records)
         self.figure.set_size_inches(w=w, h=h)
 
     def get_figure_width(self) -> float:
-        locus_width = self.width_cm_per_kb * self.max_seq_len() / 1000
-        seqname_width = self.width_cm_per_char * self.max_seqname_len()
+        locus_width = self.WIDTH_CM_PER_KB * self.max_seq_len() / 1000
+        seqname_width = self.WIDTH_CM_PER_CHAR * self.max_seqname_len()
         return locus_width + seqname_width
 
     def max_seq_len(self) -> int:
@@ -202,35 +222,4 @@ class PlotGraphicRecords(Processor):
 
     def save_output(self):
         for fmt in ['pdf']:
-            self.figure.savefig(f'{self.output}.{fmt}', dpi=self.dpi)
-
-
-class ViewLoci(Processor):
-
-    loci: List[Chromosome]
-    output: str
-
-    def __init__(self, settings: Settings):
-        super().__init__(settings=settings)
-        self.to_graphic_record = ChromosomeToGraphicRecord(self.settings).main
-
-    def main(
-            self,
-            loci: List[Chromosome],
-            output: str):
-
-        self.loci = loci
-        self.output = output
-
-        graphic_records = self.get_graphic_records()
-        self.plot(graphic_records)
-
-    def get_graphic_records(self) -> List[GraphicRecord]:
-        return [self.to_graphic_record(locus) for locus in self.loci]
-
-    def plot(self, graphic_records: List[GraphicRecord]):
-        seqnames = [locus.seqname for locus in self.loci]
-        PlotGraphicRecords(self.settings).main(
-            graphic_records=graphic_records,
-            seqnames=seqnames,
-            output=self.output)
+            self.figure.savefig(f'{self.output}.{fmt}', dpi=self.DPI)
